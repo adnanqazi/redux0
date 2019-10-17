28ff862db90a92ce83fee478b207598a57f68dcd
Adnan qazi <adnanqazi@hotmail.com>
2019-10-17 16:43:15 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
+//import { Provider } from 'react-redux'
+import reducer from './reducers'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = { tech: "React" };
+const store = createStore(reducer, initialState)
+
+class App extends Component {
+
+  render() {
+    console.log("store state is ");
+      console.log(store.getState());
+
+    return (<div>
+        <HelloWorld tech={store.getState().tech} />
+        <ButtonGroup key={2} technologies={["React", "Elm", "Reactredux"]} />
+    </div>)
+  }
+}
+
+const HelloWorld = props => <div>Hello {props.tech}</div>
+
+const setTechnology = text => ({ type: "SET_TECHNOLOGY", tech: text });
+
+const ButtonGroup = ({ technologies }) => (
+<div>
+  {technologies.map((tech, i) => (
+    <button data-tech={tech} key={`btn-${i}`} className="hello-btn" onClick={dispatchBtnAction}>
+      {tech}
+    </button>
+))}
+</div>
+);
+
+function dispatchBtnAction(e) {
+
+const tech = e.target.dataset.tech;
+console.log(`tech is ${tech}`);
+store.dispatch(setTechnology(tech));
 }
 
 export default App;
